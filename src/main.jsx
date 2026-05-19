709c01cb441d7d30d4b90d5e06051e5e906ec726
Rufino <a14891@oficina.pt>
2026-05-19 09:44:21 +0000

    tentei

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Message from './Message.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('roota')).render(
   <StrictMode>
-    <Message />
+    <Header />
     <App></App>
+    <Footer />
   </StrictMode>,
 )
