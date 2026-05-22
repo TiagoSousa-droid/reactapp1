61826ef34e397353f0a0fd6660e7b89b79ec81a6
Rufino <a14891@oficina.pt>
2026-05-22 11:51:35 +0000

    quase la

---
+++ src/Alerta.jsx
@@ -0,0 +1,17 @@
+
+function Alerta(props) {
+
+    return (
+        <div>
+            <div className="alert alert-success text-center" role="alert">
+                {props.text}
+            </div>
+        </div>
+    );
+}
+
+className={`text-center ${ props.NOVA
+}`}
+
+export default Alerta;
+
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import Mensagem from './Mensagem';
+import Alerta from './Alerta';
+
 // Um componente React é uma função que devolve JSX (JavaScript + HTML)
 function App() {
   return (
@@ -15,10 +18,17 @@ function App() {
       {/* Evento onClick em React usa camelCase e chama uma função */}
       <button
         className="btn btn-success d-block mx-auto"
-        onClick={() => alert('Olá, mundo!')}
+        onClick={() => alert('Olá, Rufino!')}
       >
         Clica-me!
       </button>
+
+      <h1 className="text-center">Componentes e Props</h1>
+      {/* Cada componente recebe props diferentes */}
+      <Mensagem mytext="Bem-vindo à tua primeira aplicação React!" />
+      <Mensagem mytext="Os componentes permitem reutilizar código!" />
+
+      <Alerta text="aiaiai" />  
     </div>
   );
 }
